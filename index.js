7352908719d185bfa3e95edfae4bc1733cfd2994
Anoop tk <anooptk3@gmail.com>
2023-11-26 10:20:10 +0530

    added

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ connection()
 // middleware
 app.use(express.json())
 // Enable CORS for a specific origin
-app.use(cors({ origin: 'https://656286dee167cf0bdd5302f6--celadon-unicorn-cc9e09.netlify.app' }));
+// app.use(cors({ origin: 'https://656286dee167cf0bdd5302f6--celadon-unicorn-cc9e09.netlify.app' }));
+app.use(cors())
 
 
 
 // routes
 app.use(morgan("dev"))
+app.get("/",(req, res) => res.send("success"))
 app.use("/auth",authmiddleware,(req, res) => res.status(200).json("success"))
 app.use("/api",Routes)
 
